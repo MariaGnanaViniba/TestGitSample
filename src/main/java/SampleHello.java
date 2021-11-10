7e5f377a37b7b893d461d9e8a09f9e42d23dfa54
mariagnanaviniba <vini.mca@gmail.com>
2021-11-10 16:52:31 +0530

    Updated

--- src/main/java/SampleHello.java
+++ src/main/java/SampleHello.java
@@ -1,5 +1,9 @@
+import sun.lwawt.macosx.CSystemTray;
+
 public class SampleHello {
     public static void main(String args[]) {
         System.out.println("Hello Java");
+        System.out.println("Test");
+        System.out.println("test Sample");
     }
 }
